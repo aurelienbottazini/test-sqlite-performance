6569af57baf85bb6041f9bbabd5ddd0a0dcb5b26
Aurélien Bottazini <aurelien.bottazini@gmail.com>
2022-10-15 22:44:40 +0200

    Refactor go to use faster libs

--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,16 @@ module example.com/helloworld
 
 go 1.18
 
-require github.com/mattn/go-sqlite3 v1.14.15 // indirect
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/bvinc/go-sqlite-lite v0.6.1 // indirect
+	github.com/fasthttp/router v1.4.12 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
+	github.com/savsgio/atreugo/v11 v11.9.7 // indirect
+	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.40.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+)
--- go/main.go
+++ go/main.go
@@ -1,68 +1,77 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
+	"strconv"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/bvinc/go-sqlite-lite/sqlite3"
+	"github.com/savsgio/atreugo/v11"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello world!\n")
-}
-
-func visit(db *sql.DB, sqlStmt string) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		_, err := db.Exec(sqlStmt)
-		if err != nil {
-			log.Printf("%q: %s\n", err, sqlStmt)
-			return
-		}
-
-		w.WriteHeader(http.StatusNoContent)
-	}
-
-}
-func stats(db *sql.DB, sqlStmt string) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		var count int
-		err := db.QueryRow(sqlStmt).Scan(&count)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Fprintln(w, count)
-	}
-
-}
-
 func main() {
-	db, err := sql.Open("sqlite3", "./analytics.sqlite3?_journal_mode=wal&_synchronous=1")
+	conn, err := sqlite3.Open("./analytics.sqlite3")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer conn.Close()
 
 	sqlStmt := `
 CREATE TABLE IF NOT EXISTS visits (
 id    INTEGER PRIMARY KEY,
 user_agent TEXT NOT NULL,
 referrer  TEXT NOT NULL);
+pragma journal_mode=wal;
+pragma synchronous=1;
 pragma page_size = 4096;
 pragma mmap_size = 30000000000;
 pragma temp_store = MEMORY;
 	`
-	_, err = db.Exec(sqlStmt)
+	err = conn.Exec(sqlStmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, sqlStmt)
 		return
 	}
 
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/visit", visit(db, "INSERT INTO visits (user_agent, referrer) VALUES ('foo', 'bar')"))
-	http.HandleFunc("/stats", stats(db, "SELECT MAX(id) FROM visits;"))
+	config := atreugo.Config{
+		Addr: "0.0.0.0:3030",
+	}
+	server := atreugo.New(config)
 
-	http.ListenAndServe("0.0.0.0:3030", nil)
+	server.GET("/hello", func(ctx *atreugo.RequestCtx) error {
+		return ctx.TextResponse("Hello World!")
+	})
+
+	server.GET("/visits", func(ctx *atreugo.RequestCtx) error {
+		err := conn.Exec("INSERT INTO visits (user_agent, referrer) VALUES ('foo', 'bar')")
+		if err != nil {
+			log.Printf("%q: %s\n", err, sqlStmt)
+		}
+
+		return ctx.TextResponse("OK")
+	})
+
+	server.GET("/stats", func(ctx *atreugo.RequestCtx) error {
+		stmt, err := conn.Prepare("SELECT MAX(id) FROM visits;")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Step()
+		if err != nil {
+			return fmt.Errorf("step failed while querying count: %v", err)
+		}
+		var count int
+		err = stmt.Scan(&count)
+		if err != nil {
+			return fmt.Errorf("scan failed while querying count: %v", err)
+		}
+
+		return ctx.TextResponse(strconv.Itoa(count))
+	})
+
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
 }
