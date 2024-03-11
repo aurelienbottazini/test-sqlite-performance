cba4bc395ea1b4aea3d12f73354b9a7bbf76f49e
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2024-03-11 22:07:04 +0100

    update rails

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-for w in clojure clojurescript go js js-bun js-deno rails ruby rust
+for w in clojure #clojurescript go js js-bun js-deno rails ruby rust
 do
   cd $w
   rm  -f ./analytics.sqlite3
@@ -9,8 +9,8 @@ do
   make build 1>/dev/null
   make serve 1>/dev/null &
   SERVER_PID=$!
-  echo "sleeping for 5 seconds to let server start"
-  sleep 5
+  echo "sleeping for 10 seconds to let server start"
+  sleep 10
   echo "hello"
   wrk -t12 -c400 -d30 http://0.0.0.0:3030/hello > ../stats/${w}_hello.txt
   echo "visit"
