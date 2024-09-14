4695bb3c466323fdf1b64280cf7c95aaae335786
Aurélien Bottazini <32635+aurelienbottazini@users.noreply.github.com>
2024-09-14 20:22:17 +0200

    rails 8

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'up/index'
   get 'stats', to: 'stats#index'
   get 'visit', to: 'visit#index'
-  get 'hello', to: 'hello#index'
+  # get 'hello', to: 'hello#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'hello#index'
+  root 'stats#index'
 end
